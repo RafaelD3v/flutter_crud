e48ac6d14b962ccf141c03c47bf155829bd3ec2d
Rafael Tavares <rafaelcontato.ce@gmail.com>
2021-03-25 21:40:49 -0300

    Finished

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com',
+    avatarUrl: '',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@alunos.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Alan',
+    email: 'alan@alunos.com',
+    avatarUrl: '',
+  ),
+};
